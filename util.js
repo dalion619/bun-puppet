273fc3d5a3e790507821ff80d6d0737b59fe12c2
LioneL Christopher Chetty <lionelcc619@gmail.com>
2024-04-05 08:39:54 +0200

    basic example

---
+++ util.js
@@ -0,0 +1,16 @@
+/**
+ * Get the URL parameters
+ * source: https://css-tricks.com/snippets/javascript/get-url-variables/
+ * @param  {String} url The URL
+ * @return {Object}     The URL parameters
+ */
+export function parseQueryParams(url) {
+    var params = {}
+    var query = url.search.substring(1)
+    var vars = query.split('&')
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=')
+        params[pair[0]] = decodeURIComponent(pair[1])
+    }
+    return params
+}
